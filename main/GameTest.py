f0b4e38b2ef4fd264c8afaad1183aa1b8571f314
dklasa <davidklasa99@gmail.com>
2025-02-28 12:20:19 +0100

    Changes to Test

---
+++ main/GameTest.py
@@ -0,0 +1,26 @@
+import random
+import pyspiel
+import numpy as np
+
+game = pyspiel.load_game("python_submarine_helicopter")
+state = game.new_initial_state()
+
+while not state.is_terminal():
+    print(state)
+    legal_actions = state.legal_actions()
+    pl = state.current_player()
+    if pl == 0:
+        pos = 0
+        flag = True
+        x = 0
+        while flag:
+            if legal_actions[x] > state.sub_pos:
+                pos = legal_actions[x]
+                flag = False
+            x += 1
+    elif pl == 1:
+        num = len(legal_actions)
+        pos = legal_actions[random.randint(1, num)]
+    state.apply_action(pos)
+
+print(state.returns())
