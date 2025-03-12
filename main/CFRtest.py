0dccaa07cfd7dded643b8ff2d20e3e79f4317717
WilliamBjornram <wsb@kth.se>
2025-03-12 12:20:58 +0100

    Update CFRtest.py

--- main/CFRtest.py
+++ main/CFRtest.py
@@ -48,7 +48,7 @@ cfr_solver = cfr.CFRSolver(game)
 # Run CFR iterations...
 eval = 5
 for i in range(11):
-  #print("HEJ")
+  print("UNO ITER")
   cfr_solver.evaluate_and_update_policy()
   if i % eval == 0:
     conv = exploitability.exploitability(game, cfr_solver.average_policy())
