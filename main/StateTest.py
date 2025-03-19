3c737e65218d96a2ea412a616823152b4e01b4c0
dklasa <davidklasa99@gmail.com>
2025-03-19 10:47:23 +0100

    Update StateTest.py

--- main/StateTest.py
+++ main/StateTest.py
@@ -1,5 +1,6 @@
 
 from open_spiel.python import games
+from open_spiel.python.algorithms.get_all_states import get_all_states
 import pyspiel
 import time
 
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     game = pyspiel.load_game("python_submarine_helicopter")
 
     start = time.time()
-    states = pyspiel.get_all_states(game, depth_limit=100, include_terminals=True, include_chance_states=True)
+    states = get_all_states(game, depth_limit=100, include_terminals=True, include_chance_states=True)
     end = time.time()
     print(f"Total tid tränat: {(end-start)/60} min")
     print("Number of states in game tree:", len(states))
