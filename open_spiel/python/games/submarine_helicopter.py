2b6edeb9a1971f70b3a5d900675f867c0eab3c2e
dklasa <davidklasa99@gmail.com>
2025-04-02 14:17:11 +0200

    Update submarine_helicopter.py

--- open_spiel/python/games/submarine_helicopter.py
+++ open_spiel/python/games/submarine_helicopter.py
@@ -120,7 +120,7 @@ class SubmarineHelicopterState(pyspiel.State):
         player = self.current_player()
       # Normaliserar timer värde
       normalized_timer = self.timer / self.budget
-      decay_factor = 0.9  # Decay factor för historik
+      decay_factor = 1.0  # Decay factor för historik
       
       # Beräknar decayed besök för historiken, ger: [pos1*0,9^2, pos2*0,9, pos3], vid tredje noden
       decayed_visits = np.zeros(len(self.graph.nodes), dtype=np.float32)
@@ -146,7 +146,7 @@ class SubmarineHelicopterState(pyspiel.State):
 
     N = len(self.graph)
     obs_size = 4 * N + 1
-    decay_factor = 0.9
+    decay_factor = 1.0
     tensor = np.zeros(obs_size, dtype=np.float32)
     # player ser bara sin egna position
     if player == 0:
