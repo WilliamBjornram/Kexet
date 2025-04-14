8f90eb041141ba11e8812c8d26e1a8c9b4755768
dklasa <davidklasa99@gmail.com>
2025-04-14 06:32:08 +0200

    CFR & MCCFR results

--- main/CFR.py
+++ main/CFR.py
@@ -22,7 +22,7 @@ from absl import app
 
 def main(_):
     # Filväg till grafen, inkludera namnet och filändelse
-    filename = "/content/Kexet/main/grafer/Graf1.csv"
+    filename = "/Users/davidklasa/Documents/GitHub/Kexet/main/grafer/Graf3.csv"
     model_data_file = "CFR_model.pkl" # filen där den tränade modelen ska sparas
     training_data_file = "CFR_average_results.csv" # filen där träningsdatan ska sparas
  
